0aa2d0b2180927383c3b12cba962b006cc16c0d9
Aryan N. Jain <aryanj1084@gmail.com>
2023-12-10 18:26:56 +0530

    Name Mobile No. Binary Search.py

--- Name Mobile No. Binary Search.py
+++ Name Mobile No. Binary Search.py
@@ -1,41 +1,31 @@
-def binary_search(arr, key):
-    
-    start = 0
-    end = len(arr)-1
-    while start <= end:
-        mid = (start + end)//2
-        if arr[mid] > key:
-            end = mid-1
-        elif arr[mid] < key:
-            start = mid + 1
+def binary(arr,key):
+    start=0
+    end=len(arr)-1
+    while (start<=end):
+        mid=(start+end)//2
+        if(arr[mid]>key):
+            end=mid-1
+        elif(arr[mid]<key):
+            start=mid+1
         else:
-            return mid
-    return -1
+            if(arr[mid]==key):
+                return(True)
+    return(False)
 
+num=int(input("Number of entries= "))
 dict1={}
-
-while True:
-    data=input("Enter name and mobile number seperated by comma")
-    if 'Exit' == data:
-        break
-    name,num=data.split(",")
-    num=int(num)
-    dict1[num]=name 
-print(dict1)
+arr=[]
+for i in range(num):
+    name=input("Enter name of person")
+    phone=int(input("Enter phone no of person"))
+    dict1[phone]=name
+    arr.append(phone)
 list1=dict1.keys()
-arr=list(list1)
-arr.sort()
-print(arr)
-
-key = int(input('The number to search for: '))
- 
-index = binary_search(arr, key)
-if index < 0:
-    print('{} was not found.'.format(key))
+list1=list(list1)
+list1=list1.sort()           
+key=int(input("Enter the phone number you want to search= "))
+val=binary(arr,key)
+if(val):
+    print("Person found. The person of phone no.",key,"is",dict1[key])
 else:
-    print('{} was found at index {}.'.format(key, index))
-    print(key," was found at index",index)
-    print('Number found at index', index)
-    print("details of number found")
-    print(dict1[key])
-
+    print("Person not found")
